8238a692ca08a59af9757f61f7d2136007502a7f
Farme7 <fabiano.alexandre@santuariocristoredentor.com.br>
2025-06-18 15:24:27 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+// Firebase App (modular)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// TODO: Substitua pelos seus dados do Firebase
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_DOMINIO.firebaseapp.com",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET.appspot.com",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Exemplo: adicionar homenagem manualmente via JS
+// await addDoc(collection(db, "homenagens"), { nome: "Maria", mensagem: "Obrigada por tudo!" });
